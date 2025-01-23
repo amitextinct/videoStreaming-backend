78b5cc35a3ca1677c10cd4327a36d57345454d92
Amit Mahto <amitextinct@gmail.com>
2025-01-23 19:35:29 +0530

    added updateComment and getAllComments

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,6 +9,72 @@ const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video ID");
+  }
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+  // if (video.comments.length < 1) {
+  //   return res.status(200).json(new ApiResponse(200, [], "No comments found"));
+  // }
+  const options = {
+    page,
+    limit,
+  };
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "createdBy",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        createdBy: {
+          $first: "$createdBy",
+        },
+      },
+    },
+    {
+      $unwind: "$createdBy",
+    },
+    {
+      $project:{
+        content: 1,
+        createdBy: 1,
+      },
+    },
+    {
+      $skip: (page - 1) * limit,
+    },
+    {
+      $limit: parseInt(limit),
+    }
+  ]);
+  if (!comments) {
+    throw new ApiError(404, "error fetching comments");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "Comments fetched successfully"));
 });
 
 const addComment = asyncHandler(async (req, res) => {
@@ -43,6 +109,39 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+  const { commentId } = req.params;
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment ID");
+  }
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+  if (comment.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not authorized to update this comment");
+  }
+  const { content } = req.body;
+  if (!content) {
+    throw new ApiError(400, "Comment content is required");
+  }
+  if (content.length < 1) {
+    throw new ApiError(400, "Comment content is too short");
+  }
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: {
+        content,
+      },
+    },
+    { new: true }
+  );
+  if (!updatedComment) {
+    throw new ApiError(400, "Failed to update comment");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedComment, "Comment updated successfully"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
